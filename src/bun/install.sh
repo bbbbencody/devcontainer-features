f867cd65dcfb0f177f54ee9e794f87fca81f0ab1
Ben Cody <bencody@Bens-MacBook-Pro.local>
2024-08-29 21:25:38 -0500

    Remove template test code and update install and test scripts

--- src/bun/install.sh
+++ src/bun/install.sh
@@ -3,10 +3,7 @@ set -e
 
 echo "Installing bun"
 
-BUN_VERSION=${BUN_VERSION:-undefined}
+curl -fsSL https://bun.sh/install | bash
 
-
-curl -fsSL https://bun.sh/install | bash -s "bun-v${BUN_VERSION}"
-
-~/.bashrc << 'export BUN_INSTALL="$HOME/.bun"'
-~/.bashrc << 'export PATH="$BUN_INSTALL/bin:$PATH"'
+~/.bashrc >> 'export BUN_INSTALL="$HOME/.bun"'
+~/.bashrc >> 'export PATH="$BUN_INSTALL/bin:$PATH"'
